716ff12875b9cbb7266208df4986b97de89879db
Rohith Krishnan <rohithk652@gmail.com>
2024-06-17 23:07:17 +0530

    added frontend and backend

---
+++ test/app/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+import Hero from "../components/ui/Hero";
+import Navbar from "../components/ui/Navbar";
+import GradientWrapper from "../components/GradientWrapper";
+import CenteredCTA from "../components/ui/CenteredCTA";
+import Stats from "../components/ui/Stats";
+import Features from "../components/ui/Features";
+import CTA from "../components/ui/CTA";
+import Pricing from "../components/ui/Pricing";
+import Testimonial from "../components/ui/Testimonial";
+import Footer from "../components/ui/Footer";
+import FooterCTA from "../components/ui/FooterCTA";
+
+export default function Home() {
+	return (
+		<div className="w-full bg-white ">
+			<Navbar />
+			<Hero />
+			<GradientWrapper className="pb-0 sm:my-16">
+				<CenteredCTA />
+				<Stats />
+			</GradientWrapper>
+			<Features />
+			<CTA />
+			<Pricing />
+			<Testimonial />
+			<FooterCTA />
+			<Footer />
+		</div>
+	);
+}
